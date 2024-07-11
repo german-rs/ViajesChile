964ef8c4b8a2820c514d80167c7c9b3872987a06
german-rs <18427568+german-rs@users.noreply.github.com>
2024-07-11 10:00:35 -0400

    Se mejora la barra de navegación (smooth scroll), quienes somos y se agrega tooltip.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,34 @@
     Jornada: Vespertina.   
     Desafío: Viajes Chile. 
 */
+
+
+$(document).ready(function () {
+
+    // Smooth scroll (Movimiento).
+    $(".nav-link").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault(); var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 2000, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+    //Smooth scroll (transparencia).
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('.navbar').addClass('navbar-transparente');
+            $('.nav-link, .navbar-brand').addClass('navlink-cambio-color');
+        } else {
+            $('.navbar').removeClass('navbar-transparente');
+            $('.nav-link, .navbar-brand').removeClass('navlink-cambio-color');
+        }
+    });
+
+    //Tooltip.
+    $('[data-bs-toggle="tooltip"]').tooltip();
+
+});
